790a31539a923608d8805fa99e3fcd77b92bfffd
LuchoRagusa <luchito.rc4@gmail.com>
2022-03-17 22:21:40 -0300

    test

---
+++ ruleta.py
@@ -0,0 +1,28 @@
+import random
+from turtle import color
+import matplotlib.pyplot as plt
+
+x = []
+y= []
+
+def funcion(rep):
+    c=0; suma=0
+    while c <= rep:
+        suma = suma + random.randint (0,36)
+        c +=1
+        x.append(c)
+        y.append(suma/c)
+    return (suma/rep)
+
+print("Ingrese la cantidad de repeticiones que quiere ejecutar: ", end=""); rep = int(input())
+
+print("El promedio es: ", funcion(rep))
+
+plt.plot(x,y, color='b')
+plt.xlabel("Nro de tiro")
+plt.ylabel("Resultado del tiro")
+plt.title("Simulacion ruleta")
+
+#plt.savefig('grafico.png')
+
+plt.show()
