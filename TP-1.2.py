5301079bae74b5ec40b156641183b75bee3c6f02
JuaniPucheta <80986033+JuaniPucheta@users.noreply.github.com>
2022-03-29 16:55:14 -0300

    matrizApuestas

--- TP-1.2.py
+++ TP-1.2.py
@@ -18,6 +18,8 @@ matrizR = [[1,4,7,10,13,16,19,22,25,28,31,34]
         ,[2,5,8,11,14,17,20,23,26,29,32,35]
         ,[3,5,9,12,15,18,21,24,27,30,33,36]]
 
+numerosRojos = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+
 # Definir apuesta por columna y por docena, ademas hacer que se pueda apostar por muchos a la vez
 # Modificar la opcion 1 para que sea solo numero, y que se pueden apostar a varias opciones
 
@@ -72,8 +74,9 @@ def main():
                 continue
 
             print(explicacion_ruleta)
-            dinero_apostado = solicitarDineroRuleta(saldo_global)
             eleccion_ruleta = ""
+            matrizApuestas = [[], 
+                                []]
             while eleccion_ruleta != "7":
                 print(f"Dinero disponible: {saldo_global}")
                 eleccion_ruleta = input(""" 1. Número
@@ -86,15 +89,16 @@ def main():
                                             Elige: """)
 #Opcion 1 (Número)
                 if eleccion_ruleta == "1":
-                    
+                    seguirApostando = True
+
                     if(validaSaldo(saldo_global)):
                         break
-                    
+                
+                while seguirApostando:
                     numero_usuario = pedirNumero()
-                    
-                    #Se elige aleatoriamente el numero
-                    nRandom = random.randint(0, 36)
-                    print("Numero obtenido: " + str(nRandom))
+                    dinero_apostado = solicitarDineroRuleta(saldo_global)
+                    matrizApuestas.append([numero_usuario],[dinero_apostado])
+
 
                     if nRandom == numero_usuario:
                         #Acierta numero
