28e6c075b7a36662a8c414a969ba2b2559601d44
LuchoRagusa <luchito.rc4@gmail.com>
2022-04-06 11:15:26 -0300

    martinGala ok

--- testMartinGala.py
+++ testMartinGala.py
@@ -7,10 +7,11 @@ import matplotlib.pyplot as plt
 from pyparsing import col 
 from scipy import stats # importando scipy.stats
 
-apuesta_minima_ruleta = 1000
+apuesta_minima_ruleta = 1
 rojo = "Rojo"
 negro = "Negro"
-jugadas = []
+evolucionApuesta = []
+evolucionSaldo = []
 numerosRojos = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]
 
 
@@ -22,18 +23,15 @@ menu = """###### MENU RULETA #######
 2. SALIR
 Elige: """
 
-def solicitarDineroRuleta(saldoGlobal):
+def solicitarDineroRuleta():
     dinero_apostado = 0
-    if  saldoGlobal >= apuesta_minima_ruleta:
-        dinero_apostado = float(input(f"¿Cuanto apostás a la martin gala? Debe ser al menos '{apuesta_minima_ruleta}' pesos, y no debe superar el saldo: "))
-        band = True
-        while band:
-            if(dinero_apostado >= apuesta_minima_ruleta and dinero_apostado <= saldoGlobal):
-                band = False
-            else:
-                dinero_apostado = float(input(f"Valor no valido, ingresa una apuesta valida: "))
-    else:
-        print("Tu saldo no llega al minimo para apostar.")
+    dinero_apostado = float(input(f"¿Cuanto apostás a la martin gala? Debe ser al menos '{apuesta_minima_ruleta}' pesos, y no debe superar el saldo: "))
+    band = True
+    while band:
+        if(dinero_apostado >= apuesta_minima_ruleta ):
+            band = False
+        else:
+            dinero_apostado = float(input(f"Valor no valido, ingresa una apuesta valida: "))
     return dinero_apostado
 
 def pedirNumero():
@@ -46,52 +44,53 @@ def pedirNumero():
         else:
             band = True
 
-def validaSaldo(saldo):
-    band = False
-    if saldo < apuesta_minima_ruleta:
-        print(f"Necesitas de al menos {apuesta_minima_ruleta} pesos para jugar.")
-        band = True
-    return band
-
 def main():
-    saldo_global = 50000 #El saldo con el que se inicia
     eleccion = ""
+    colorSeleccionado = ""
     while eleccion != "2":
         eleccion = input(menu)
-        for i in range(3):
-            print(f"Dinero disponible de su cuenta: {saldo_global}")
-            if(validaSaldo(saldo_global)):
-                continue
-            matrizApuestas = []
-            contadorMG = 0
-            if(validaSaldo(saldo_global)):
-                break
-            dinero_apostado = solicitarDineroRuleta(saldo_global)
-            if (dinero_apostado == 0):
-                break
-            color_eleccion_usuario = input("\t1.Rojo\n\t2.Negro\n\tElige: ")         #Para UN SOLO color
-            if color_eleccion_usuario == "1":
-                matrizApuestas.append([rojo, dinero_apostado])
-            elif color_eleccion_usuario == "2":
-                matrizApuestas.append([negro, dinero_apostado])
-            else:
-                print("Ingresó otra opción.")
-                break
+        
+        dinero_apostado = solicitarDineroRuleta()
+
+        color_eleccion_usuario = input("\t1.Rojo\n\t2.Negro\n\tElige: ")         #Para UN SOLO color
+        if color_eleccion_usuario == "1":
+            colorSeleccionado = rojo
+        elif color_eleccion_usuario == "2":
+            colorSeleccionado = negro
+        else:
+            print("Ingresó otra opción.")
+            break
 #Opcion 10 girar
-        matrizInd = matrizApuestas[contadorMG]
-        while (matrizInd[1] <= saldo_global):
-            nRandom = ran.randint (0,36)
-            saldo_global -= matrizInd[1] # le descuento al salgoGlobal lo apostado
-            if(nRandom in numerosRojos and matrizInd[0] == rojo) or (nRandom not in numerosRojos and matrizInd[0] == negro):
-                saldo_global += matrizInd[1]*2
-                matrizApuestas.append([matrizInd[0], dinero_apostado])
-                print(matrizInd, " Ganaste, el saldo global es: ", saldo_global)    
-            else:
-                matrizApuestas.append([matrizInd[0], matrizInd[1]*2])
-                print(matrizInd, " Perdiste, el saldo global es: ", saldo_global) 
-            contadorMG += 1
+        for i in range(1):
+            matrizApuestas = []
+            saldo_global = 10 #El saldo con el que se inicia
+            mSaldo = []
+            mSaldo.append(saldo_global)
+            matrizApuestas.append([colorSeleccionado, dinero_apostado])
+            contadorMG = 0
             matrizInd = matrizApuestas[contadorMG]
-        print("Te quedaste sin saldo pa")
+
+            while (matrizInd[1] <= saldo_global):
+                nRandom = ran.randint (0,36)
+                saldo_global -= matrizInd[1] # le descuento al salgoGlobal lo apostado
+                if(nRandom in numerosRojos and matrizInd[0] == rojo) or (nRandom not in numerosRojos and matrizInd[0] == negro):
+                    saldo_global += matrizInd[1]*2
+                    matrizApuestas.append([matrizInd[0], dinero_apostado])
+                    print(matrizInd, " Ganaste, el saldo global es: ", saldo_global)    
+                else:
+                    matrizApuestas.append([matrizInd[0], matrizInd[1]*2])
+                    print(matrizInd, " Perdiste, el saldo global es: ", saldo_global) 
+                mSaldo.append(saldo_global)
+                contadorMG += 1
+                matrizInd = matrizApuestas[contadorMG]
+            print("Te quedaste sin saldo pa")
+
+            evolucionApuesta.append(matrizApuestas)
+            evolucionSaldo.append(mSaldo)
+
+            print("Matriz de apuesta: ", evolucionApuesta) # tener en cuenta que la ultima apuesta es la q no se hace pq no tenes saldo disponible
+            print("Matriz de saldos: ", evolucionSaldo)
+
+            print(f"Saldo final: {saldo_global}") 
         print(f"Saldo final: {saldo_global}") 
-    print(f"Saldo final: {saldo_global}") 
 main()
