c120f2656243de310b70c253237c8ac7ffe8a0e4
JuaniPucheta <80986033+JuaniPucheta@users.noreply.github.com>
2022-05-05 22:53:54 -0300

    gcl todo en uno

--- TP-2.1/GCL.py
+++ TP-2.1/GCL.py
@@ -23,80 +23,95 @@ import matplotlib.pyplot as plt
     Llegará un momento en que el resultado obtenido coincida con alguno de los resultados ya obtenidos. Cuando eso pase
         sabremos que la serie ha acabado y a continuacion se repetirán los mismos números. Por ejemplo con m=7, a=5, c=3 y x0=2
 '''
-
-
-
-def barrasGCL():
-    xn = int(time.time()) #Semilla
-    cantNumA = []
-    x=[]
-    for j in range(100, 150):
-        numerosGenerados = []
-        x.append(j)
-        for i in range(40000):
-            xn1 = (1103515245 * xn + 12345) % (j)
-            xn = xn1    #xn es la nueva semilla    
-            if xn1 in numerosGenerados:
-                break
-            numerosGenerados.append(xn1)
-        print("Ya analizo el módulo: ", j)
-        cantNumA.append(len(numerosGenerados))
-    print(cantNumA)
-    plt.bar(x, cantNumA)    #Grafica de barras
-    plt.grid(True)
-    plt.xlabel("Modulo")
-    plt.ylabel("Cantidad de n generados")
-    plt.title("Evaluacion de n generados de acuerdo al modulo")
-    plt.show()
-#EXPLICACION: un modulo grande no quiere decir muchos numeros aleatorios, no es directamente proporcional
-#sin embargo, con el valor de modulo en el que mas numeros aleatorios son generados, sin repeticion, es 32768.
-#y la cantidad de numeros generados es 32768.
-barrasGCL()
-
-def scatterGCL():
-    xn = int(time.time()) #Semilla
-    numerosGenerados = []
-    x = []; y = []
-    for i in range(1000):
-        xn1 = (1103515245 * xn + 12345) % 32768
-        xn = xn1    #xn es la nueva semilla    
-        if xn1 in numerosGenerados:
-            print(f"Se repite en posición del for: '{i}' con valor: '{xn1}'")
-            break
-        numerosGenerados.append(xn1)
-        x.append(i+1)
-        y.append(xn1)
-        plt.scatter(x,y)
-    plt.xlabel("Numero de repeticiones")
-    plt.ylabel("Resultados GCL")
-    plt.title("Evaluacion de GCL")
-    plt.show()
-scatterGCL()
-
-def segundoGCL():
-    xn = int(time.time())
-    total = []
-    repetido = False
-    while (not repetido):
-        xn1 = (1103515245 * xn + 12345) % 32768
-        xn = xn1 
-        repetido = xn1 in total
-        if (not repetido):
-            total.append(xn1)
-    print(total)
-    print(len(total))
-#segundoGCL()
-
-def decimalGCL():
-    xn = int(time.time())
-    for i in range(10):
-        xn1 = ((1103515245 * xn + 12345) % 32768) / 32768.00    #los .00 son un "truco" para imprimir el resultado con decimales
-        print(xn1)
-        xn = xn1
-#decimalGCL()
+def GCL():
+    eleccion_Grafica = ""
+    print("==============================================")
+    print("             MENÚ DE GRAFICOS GCL's           ")
+    print("==============================================")
+    while eleccion_Grafica != "5":
+        eleccion_Grafica = input("\n\t1. BARRAS\n\t2. SCATTER BUENO\n\t3. SCATTER MALO\n\t4. PLOT\n\t5. Salir\n\tElija: ")
+        if eleccion_Grafica == "1":
+            ''' 
+                EXPLICACION: un modulo grande no quiere decir muchos numeros aleatorios, no es directamente proporcional
+                    sin embargo, con el valor de modulo en el que mas numeros aleatorios son generados, sin repeticion, es 32768.
+                    y la cantidad de numeros generados es 32768.                  
+            '''
+            xn = int(time.time()) #Semilla
+            cantNumA = []
+            x=[]
+            for j in range(100, 150):
+                numerosGenerados = []
+                x.append(j)
+                for i in range(40000):
+                    xn1 = (1103515245 * xn + 12345) % (j)
+                    xn = xn1    #xn es la nueva semilla    
+                    if xn1 in numerosGenerados:
+                        break
+                    numerosGenerados.append(xn1)
+                print("Ya analizo el módulo: ", j)
+                cantNumA.append(len(numerosGenerados))
+            print(cantNumA)
+            plt.bar(x, cantNumA)    #Grafica de barras
+            plt.grid(True)
+            plt.xlabel("Modulo")
+            plt.ylabel("Cantidad de numeros generados")
+            plt.title("Evaluacion de numeros generados de acuerdo al modulo")
+            plt.show()
+        elif eleccion_Grafica == "2":
+            xn = int(time.time()) #Semilla
+            numerosGenerados = []
+            x = []; y = []
+            for i in range(1000):
+                xn1 = (1103515245 * xn + 12345) % 32768
+                xn = xn1    #xn es la nueva semilla    
+                if xn1 in numerosGenerados:
+                    print(f"Se repite en posición del for: '{i}' con valor: '{xn1}'")
+                    break
+                numerosGenerados.append(xn1)
+                x.append(i+1)
+                y.append(xn1)
+                plt.scatter(x,y)
+            plt.xlabel("Numero de repeticiones")
+            plt.ylabel("Numeros aleatorios generados")
+            plt.title("Mapa de numeros aleatorios con GCL")
+            plt.show()
+        elif eleccion_Grafica == "3":
+            xn = 5 #Semilla
+            numerosGenerados = []
+            x = []; y = []
+            for i in range(1000):
+                xn1 = (6 * xn + 7) % 23
+                xn = xn1    #xn es la nueva semilla    
+                if xn1 in numerosGenerados:
+                    print(f"Se repite en posición del for: '{i}' con valor: '{xn1}'")
+                    break
+                numerosGenerados.append(xn1)
+                x.append(i+1)
+                y.append(xn1)
+                plt.scatter(x,y)
+            plt.xlabel("Numero de repeticiones")
+            plt.ylabel("Numeros aleatorios generados")
+            plt.title("Mapa de numeros aleatorios con GCL")
+            plt.show()
+        elif eleccion_Grafica == "4":
+            xn = 165 #Semilla cualquiera
+            numerosGenerados = []
+            x = []; y = []
+            for i in range(538):
+                xn1 = (2 * xn + 0) % 199
+                xn = xn1   
+                numerosGenerados.append(xn1)
+                x.append(i+1)
+                y.append(xn1)
+            plt.plot(x,y, marker='o')
+            plt.xlabel("Numero de repeticiones")
+            plt.ylabel("Numeros aleatorios generados")
+            plt.title("Evaluacion de GCL")
+            plt.show() #Link: https://demonstrations.wolfram.com/LinearCongruentialGenerators/
+GCL()
 
 #===============================================================================================
-#                                            OTRO GCL
+#                                            OTROS GCL
 #===============================================================================================
 def ingresarGCL():
     semilla = int(input("Ingrese la semilla: "))
@@ -117,3 +132,11 @@ def ingresarGCL():
         print(arreglo)
         print(len(arreglo))
 #ingresarGCL()
+
+def decimalGCL():
+    xn = int(time.time())
+    for i in range(10):
+        xn1 = ((1103515245 * xn + 12345) % 32768) / 32768.00    #los .00 son un "truco" para imprimir el resultado con decimales
+        print(xn1)
+        xn = xn1
+#decimalGCL()
