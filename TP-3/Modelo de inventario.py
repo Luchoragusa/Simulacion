fba9e86744a49be25e675e8f81586a96a1117f5c
Luchoragusa <luchito.rc4@gmail.com>
2022-05-27 15:31:04 -0300

    modelo inv

---
+++ TP-3/Modelo de inventario.py
@@ -0,0 +1,11 @@
+## https://www.youtube.com/watch?v=Kmu9DNQamLw
+## https://www.youtube.com/watch?v=7LuN_6m7h2o
+## https://towardsdatascience.com/make-your-inventory-simulation-in-python-9cb950da8cf3
+## https://github.com/sethamit71/Simulation-Optimization-of-Inventory-Management- 
+## https://github.com/khashayar-h/InventorySystem
+
+## Otros
+
+## https://github.com/wamuir/simpy-stockout
+## https://github.com/safayetsohan/Simulation-of-Inventory-System-Newspaper-/blob/master/Simulation%20of%20Inventory%20System(Newspaper).py
+## https://github.com/Samialabed52/Inventory-simulation/blob/main/HW2_Q2.py
