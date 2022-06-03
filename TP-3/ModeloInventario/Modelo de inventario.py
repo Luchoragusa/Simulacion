797a8cf9f358c3e7b589a420201bdbeea054a119
Luchoragusa <luchito.rc4@gmail.com>
2022-06-02 23:30:54 -0300

    graficas modelo inv

--- TP-3/ModeloInventario/Modelo de inventario.py
+++ TP-3/ModeloInventario/Modelo de inventario.py
@@ -1,36 +1,17 @@
-## https://www.youtube.com/watch?v=Kmu9DNQamLw
-## https://www.youtube.com/watch?v=7LuN_6m7h2o
-## https://towardsdatascience.com/make-your-inventory-simulation-in-python-9cb950da8cf3
-## https://github.com/sethamit71/Simulation-Optimization-of-Inventory-Management- 
-## https://github.com/khashayar-h/InventorySystem
-
-## Otros
-
 ## https://github.com/wamuir/simpy-stockout
-## https://github.com/safayetsohan/Simulation-of-Inventory-System-Newspaper-/blob/master/Simulation%20of%20Inventory%20System(Newspaper).py
-## https://github.com/Samialabed52/Inventory-simulation/blob/main/HW2_Q2.py
-
-#!/usr/bin/env python3
 
 import itertools
 import simpy
 import numpy
 import matplotlib.pyplot as plt
 
-"""
-Discrete-Event Simulation of Single-Product inventario System (s,S)
-
-This is a SimPy replication of the C program given by Law and Kelton
-(2000, pp. 73--79) to simulate a single-product inventario system.
-
-William Muir (2019)
-
-"""
-
 evolucionInventario = []
 evolucionDemanda= []
 evolucionInventarioT=[]
 
+l_costo_de_orden=[]
+l_area_faltante=[]
+l_area_matenimiento=[]
 
 class Inventario(object):
 
@@ -73,6 +54,9 @@ class Inventario(object):
             self._db["costo_de_orden"] += (
                 setup_cost + incremental_cost * order.size
             )
+
+            l_costo_de_orden.append(self._db["costo_de_orden"])
+
             yield self._env.timeout(order.lag)
             self.recibirPedido(order.size)
             self._openOrder = False
@@ -86,10 +70,16 @@ class Inventario(object):
             self._db["area_faltante"] -= (
                 self.level * time_since_last_event
             )
+
+            l_area_faltante.append(self._db["area_faltante"])
+
         elif self.level > 0:
             self._db["area_matenimiento"] += (
                 self.level * time_since_last_event
             )
+
+            l_area_matenimiento.append(self._db["area_matenimiento"])
+
         self._lastEvent = self._env.now
         evolucionInventarioT.append(self._lastEvent) ## aca se guardan los tiempos de los pedidos
 
@@ -184,7 +174,7 @@ if __name__ == "__main__":
         ),
         tiempo_demanda=0.10, ## Esto es el tiempo entre demandas
         rango_retraso_entrega=(0.50, 1.00), ## Tiempo de demora de la entrega, tiene distrubicion uniforme
-        tamano_simulacion=120, ## Cantidad de x de la simulacion
+        tamano_simulacion=20, ## Cantidad de x de la simulacion
         K=32.0, ## Es el precio del pedido en si, seria como una especia de evio
         i=3.0, ## Es el precio por unidad pedida
         h=1.0,
@@ -194,7 +184,8 @@ if __name__ == "__main__":
 
     politicas = [ ## Son las politicas que menciona el ejericicio, osea evalua "s" contra "S", se deja fija "s" en 20 y se evaluan los valores de 20 en 20 de "S" hasta 100
         dict(minimum=s, target=S)
-        for s, S in itertools.product([20, 40, 60], [40, 60, 80, 100])
+        ##for s, S in itertools.product([20, 40, 60], [40, 60, 80, 100])
+        for s, S in itertools.product([20], [40]) ## dependiente la cantidad de politicas son las corridas del ejercicio
         if s < S
     ]
 
@@ -243,9 +234,11 @@ if __name__ == "__main__":
             evaluation_generator(env, inventario, politica)
         )
         env.run(until=parametros["tamano_simulacion"])
+
         report(row_format, parametros, politica, db)
 
         plt.bar(evolucionInventarioT, evolucionInventario)
+        plt.title("Evolucion del stock del inventario")
         plt.xlabel("Timepo de pedidos.")
         plt.ylabel("Estado del inventario.")  
         plt.axhline(y=0, color = "black")  
@@ -253,10 +246,43 @@ if __name__ == "__main__":
         plt.axhline(y=(numpy.mean(evolucionInventario)), color = "yellow", linestyle = "dotted")
         ## plt.title("Evaluacion de la varianza sobre el conjunto de valores aleatorios")
         ## plt.ylim([70, 150]) # esta ponderada la medicion
+
         plt.show()
         x = []
         for i in range(len(evolucionDemanda)):
             x.append(i)
         plt.bar(x, evolucionDemanda)
+        plt.title("Evolucion de la demanada")
+        plt.ylabel("Cantidad de productos demandados")  
         plt.axhline(y=(numpy.mean(evolucionDemanda)), color = "yellow", linestyle = "dotted")
+        plt.show()
+
+        x = []
+        y = []
+        for i in range(len(l_costo_de_orden)):
+            x.append(i)
+            y.append(l_costo_de_orden[i]/parametros["tamano_simulacion"])
+        plt.title("Evolucion del costo de orden")
+        plt.ylabel("Costo de orden.") 
+        plt.plot(x, y)
+        plt.show()
+
+        x = []
+        y = []
+        for i in range(len(l_area_faltante)):
+            x.append(i)
+            y.append((l_area_faltante[i] * parametros["pi"])/parametros["tamano_simulacion"])
+        plt.title("Evolucion del costo de faltantes")
+        plt.ylabel("Costo de faltante.") 
+        plt.plot(x, y)
+        plt.show()
+
+        x = []
+        y = []
+        for i in range(len(l_area_matenimiento)):
+            x.append(i)
+            y.append((l_area_matenimiento[i] * parametros["h"]) /parametros["tamano_simulacion"])
+        plt.title("Evolucion del costo de mantenimiento")
+        plt.ylabel("Costo del mantenimiento.") 
+        plt.plot(x, y)
         plt.show()
