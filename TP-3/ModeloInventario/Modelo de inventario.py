38c011ec3d59e9fcfbeb310950362c3ec62bba07
Juani <juanicampora@gmail.com>
2022-06-03 16:20:22 -0300

    Comentarios

--- TP-3/ModeloInventario/Modelo de inventario.py
+++ TP-3/ModeloInventario/Modelo de inventario.py
@@ -103,7 +103,7 @@ class Demand(object):
 
 def demand_generator(env, inventario, parametros):
     """
-    Generador de numeros para al demanda con una distribucion exponencial, eso es lo que nosotros agarrabamos un numero
+    Generador de numeros para la demanda con una distribucion exponencial, eso es lo que nosotros agarrabamos un numero
     y lo metiamos en la linea divida para determinar la demana que se hace.
     """
     sizes = [i + 1 for i in range(parametros["cantidad_max_demanda"])]
@@ -165,7 +165,7 @@ if __name__ == "__main__":
     """
     parametros = dict(
         cantidad_inicial_inventario=60, ## Esto es el inventario inicial
-        cantidad_max_demanda=4, ## Esto es el numero de demandas que se pueden hacer, osea la cantidad de rangos entre los que tiene que caer le numero aleatorio
+        cantidad_max_demanda=4, ## Esto es el numero de demandas que se pueden hacer, osea la cantidad de rangos entre los que tiene que caer el numero aleatorio
         niveles_demanda=( ## Esto es la funcion de distribucion de demandas, osea los rangos de los numeros aleatorios
             0.167,
             0.500,
@@ -175,10 +175,10 @@ if __name__ == "__main__":
         tiempo_demanda=0.10, ## Esto es el tiempo entre demandas
         rango_retraso_entrega=(0.50, 1.00), ## Tiempo de demora de la entrega, tiene distrubicion uniforme
         tamano_simulacion=20, ## Cantidad de x de la simulacion
-        K=32.0, ## Es el precio del pedido en si, seria como una especia de evio
+        K=32.0, ## Es el precio del pedido en si, seria como una especie de envio
         i=3.0, ## Es el precio por unidad pedida
-        h=1.0,
-        pi=5.0,
+        h=1.0, # Costo de Holding, es decir, de mantener el articulo guardado, por el alquiler, mantenimiento y esas cosas
+        pi=5.0, # Costo de Backlog (items pedidos no recibidos)
         numero_politicas=9, ## Seria el numero de politicas
     )
 
