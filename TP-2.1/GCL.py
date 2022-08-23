8e7aedd9a7e50522be1f6a6f3786f6668d05f95a
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2022-08-23 09:42:12 -0300

    comentarios tp2-2.1 y gcl

--- TP-2.1/GCL.py
+++ TP-2.1/GCL.py
@@ -8,23 +8,23 @@ from prettytable import PrettyTable
 #===============================================================================================
 #                                               GCL
 #===============================================================================================
-#Link de donde lo saqué: https://www.victoriglesias.net/algoritmo-de-generacion-de-numeros-pseudoaleatorios/
 '''
-    Se eligen cuatro números enteros: 
-        - un modulo 'm'
-        - un multiplicador 'a'
-        - el incremento 'c'
-        - la semilla 'x0'
-    Estos numeros deben ser positivos mayores de 0. 
-    Para 'a' se define = 1103515235 y m = 32768 segun POSIX,    [JUSTO CON ESE m, es lo que mayor cantidad de tiradas da]
-        1. 1103515245 y 32768 son primos entre si.
-        2. El único factor primo de 32768 es 2: 32768 = 215 y 1103515244 | 2 Además es eficiente al ser potencia de 2.
-        3. Se cumple tanto 32768 | 4 como 1103515244 | 4
-    Una semilla valida la pdoemos obtener del reloj interno de la CPU: el RTC.
-    La formula es X(n+1) = (aXn + c) mod m
+    Explicacion:
+        Se eligen cuatro números enteros: 
+            - un modulo 'm'
+            - un multiplicador 'a'
+            - el incremento 'c'
+            - la semilla 'x0'
+        Estos numeros deben ser positivos mayores de 0. 
+        Para 'a' se define = 1103515235 y m = 32768 segun POSIX,    [JUSTO CON ESE m, es lo que mayor cantidad de tiradas da]
+            1. 1103515245 y 32768 son primos entre si.
+            2. El único factor primo de 32768 es 2: 32768 = 215 y 1103515244 | 2 Además es eficiente al ser potencia de 2.
+            3. Se cumple tanto 32768 | 4 como 1103515244 | 4
+        Una semilla válida la podemos obtener del reloj interno de la CPU: el RTC.
+        La formula es X(n+1) = (aXn + c) mod m
 
-    Llegará un momento en que el resultado obtenido coincida con alguno de los resultados ya obtenidos. Cuando eso pase
-        sabremos que la serie ha acabado y a continuacion se repetirán los mismos números. Por ejemplo con m=7, a=5, c=3 y x0=2
+        Llegará un momento en que el resultado obtenido coincida con alguno de los resultados ya alcanzados. Cuando eso pase
+            sabremos que la serie ha acabado y a continuacion se repetirán los mismos números. Por ejemplo con m=7, a=5, c=3 y x0=2
 '''
 def GCL():
     eleccion_Grafica = ""
@@ -32,23 +32,23 @@ def GCL():
     print("             MENÚ DE GRAFICOS GCL's           ")
     print("==============================================")
     while eleccion_Grafica != "5":
-        eleccion_Grafica = input("\n\t1. BARRAS\n\t2. SCATTER BUENO\n\t3. SCATTER MALO\n\t4. PLOT\n\t5. SALIR\n\tElija: ")
+        eleccion_Grafica = input("\n\t1. BARRAS\n\t2. GOOD SCATTER\n\t3. BAD SCATTER\n\t4. PLOT\n\t5. SALIR\n\tElija: ")
         if eleccion_Grafica == "1":
             ''' 
                 EXPLICACION: un modulo grande no quiere decir muchos numeros aleatorios, no es directamente proporcional
                     sin embargo, con el valor de modulo en el que mas numeros aleatorios son generados, sin repeticion, es 32768.
                     y la cantidad de numeros generados es 32768.                  
             '''
-            xn = int(time.time()) #Semilla
+            xn = int(time.time()) #Semilla 
             cantNumA = []
             x=[]
-            for j in range(100, 150):
+            for j in range(100, 150):   #Cantidad de numeros aleatorios a generar por cada modulo (m) 
                 numerosGenerados = []
                 x.append(j)
-                for i in range(40000):
-                    xn1 = (1103515245 * xn + 12345) % (j)
+                for i in range(40000):  #40000 es la cantidad de numeros aleatorios que se generan
+                    xn1 = (1103515245 * xn + 12345) % (j) 
                     xn = xn1    #xn es la nueva semilla    
-                    if xn1 in numerosGenerados:
+                    if xn1 in numerosGenerados: 
                         break
                     numerosGenerados.append(xn1)
                 print("Ya analizo el módulo: ", j)
@@ -60,7 +60,7 @@ def GCL():
             plt.ylabel("Cantidad de numeros generados")
             plt.title("Evaluacion de numeros generados de acuerdo al modulo")
             plt.show()
-        elif eleccion_Grafica == "2":
+        elif eleccion_Grafica == "2":   # No se puso en el latex
             xn = int(time.time()) #Semilla
             numerosGenerados = []
             x = []; y = []
@@ -78,7 +78,7 @@ def GCL():
             plt.ylabel("Numeros aleatorios generados")
             plt.title("Mapa de numeros aleatorios con GCL")
             plt.show()
-        elif eleccion_Grafica == "3":
+        elif eleccion_Grafica == "3":   # No se puso en el latex
             xn = 5 #Semilla
             numerosGenerados = []
             x = []; y = []
@@ -109,12 +109,12 @@ def GCL():
             plt.plot(x,y, marker='o')
             plt.xlabel("Numero de repeticiones")
             plt.ylabel("Numeros aleatorios generados")
-            plt.title("Evaluacion de GCL")
-            plt.show() #Link: https://demonstrations.wolfram.com/LinearCongruentialGenerators/
+            plt.title("Evaluacion de repetición de valores a partir de X semilla")
+            plt.show() 
 #GCL()
 
 #===============================================================================================
-#                                            OTROS GCL
+#                                            OTROS GCL e HiperPlano
 #===============================================================================================
 def ingresarGCL():
     semilla = int(input("Ingrese la semilla: "))
@@ -136,22 +136,20 @@ def ingresarGCL():
         print(len(arreglo))
 #ingresarGCL()
 
-
-
- ## Metodo del gcl
+# Metodo del HiperPlano
 def gclHiperPlano():  
-    xn = int(time.time()) #Semilla   
+    xn = int(time.time())  
     numerosGenerados = []
     for i in range(32000):
         xn1 = (1103515245 * xn + 12345) % 32768
-        xn = xn1    #xn es la nueva semilla    
+        xn = xn1     
         if xn1 in numerosGenerados:
             print(f"Se repite en posición del for: '{i}' con valor: '{xn1}'")
             break
         numerosGenerados.append(xn1)
     return numerosGenerados
 
- ## Metodo del xorshift
+# Metodo del xorshift
 def xorshiftHiperPlano():  
     x = 123456789
     y = 362436069
@@ -180,17 +178,26 @@ def plotHiperPlanoXorshift():
     ax.set_zlabel("Z-axis")
     plt.title("Hiperplano")
     plt.show()
+plotHiperPlanoXorshift()
 
-#plotHiperPlanoXorshift()
-
+#===============================================================================================
+#                                         COMPARACION DE GLC'S
+#===============================================================================================
 def testComparacion():
     '''
-        En estadística y análisis preliminar de datos, las pruebas de aleatoriedad (o test de aleatoriedad), son pruebas estadísticas usadas para decidir si una determinada muestra o conjuntos de datos responde a un patrón o puede considerarse aleatoria
         El período de un generador congruencial se define como la longitud de un ciclo y dado que la semilla depende del modulo,
             entonces el período máximo puede ser, a lo súmo, su módulo 'm'. En ese caso, el GCL tiene un periodo completo.
         Aunque pasa las pruebas de aleatoridad, es sensible a la eleccion de parametros m, a, c.
-        No deberian ser usados en aplicaciuones para las que se requiera aleatoridad de alta calidad.
+        No deberian ser usados en aplicaciones para las que se requiera aleatoridad de alta calidad.
         Se puede ver que la secuencia obtenida no es tan aleatoria y no llena todo el espacio de manera uniforme. Los parámetros del método de congruencia lineal son importantes.
+    '''
+    '''
+        Este metodo permite mostrar, mediante una grafica de barras, la cantidad de numeros generados por MGNPA.
+        Suponemos que el 0x... es la semilla del metodo xorshift.
+        En la grafica, se puede apreciar la gran cantidad de numeros generados por XOR a comparacion del resto, dependiendo la 
+            semilla que se de y el modulo. No es posible una completa comparación porque el MS utiliza como semilla 4 digitos,
+            el GLC utiliza el modulo (cosa que el resto), y eso hace variar la cant num generados, y no la semilla,
+            y en el XOR suponemos que la semilla es 0x...
     '''
     cantNGen = []
     semilla = 1651
@@ -213,13 +220,13 @@ def testComparacion():
     print(f"En el MiddleSquare con la Semilla: {semilla} la cantidad de numeros generados es: {len(historial)}")
     cantNGen.append(len(historial))
 
-##GCL // sea la semilla q sea siempre es hasta 32768, ese es el  numero que deberiamos modificar
+##GCL 
     xn = semilla #Semilla
     numerosGenerados = []
     while xn not in numerosGenerados:
         if numero != semilla:
             numerosGenerados.append(xn)
-        xn = (1103515245 * xn + 12345) % 32768
+        xn = (1103515245 * xn + 12345) % 32768 # sea la semilla que sea, siempre es hasta 32768, ese es el numero que deberiamos modificar
     print(f"En el CGL con la Semilla: {semilla} la cantidad de numeros generados es: {len(numerosGenerados)}")
     cantNGen.append(len(numerosGenerados))
 
@@ -245,16 +252,4 @@ def testComparacion():
     plt.title("Evaluacion de numeros generados de acuerdo al GNPA")
     plt.show()
 
-# 2 barras
-    #plt.bar([1,3], cantNGen) ## hay q comentar la linea 224
-    #plt.show()
-
-# Este metodo permite mostrar, mediante una grafica de barras, la cantidad de numeros generados por MGNPA.
-# Suponemos que el 0x... es la semilla del metodo xorshift.
-# En la grafica, se puede apreciar la gran cantidad de numeros generados por XOR a comparacion del resto, dependiendo la 
-    # semilla que se de y el modulo. No es posible una completa comparación porque el MS utiliza como semilla 4 digitos,
-    # el GLC utiliza el modulo (cosa que el resto), y eso hace variar la cant num generados, y no la semilla,
-    # y en el XOR suponemos que la semilla es 0x...
-
-# PONER EN LA CONCLUSION EL LINK DEL UNICO CODIGO QUE GENERA BITMAP XORSHIFT (C++): https://codingforspeed.com/using-faster-psudo-random-generator-xorshift/
 testComparacion() 
