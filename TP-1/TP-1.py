e9811579ec96d94c6ef00d123063503d4715fb30
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2022-08-23 16:15:19 -0300

    comentarios TP-1

--- TP-1/TP-1.py
+++ TP-1/TP-1.py
@@ -1,44 +1,38 @@
-from cmath import sqrt
-import numpy as np # importando numpy
+import numpy as np 
 import random as ran
-from turtle import color
 import matplotlib.pyplot as plt
-from pyparsing import col 
-from scipy import stats # importando scipy.stats
 
 conjuntoValores = []
 conjuntoValoresHi = []
 
-def funcion(rep, corr):
+def funcion(rep, corr): 
     for c in range(corr):
         valoresInt = []
-        hi =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        hi =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] # arreglo de frecuencias relativas de cada numero de la ruleta
         for i in range(rep):
-            nRandom = ran.randint (0,36)
-            valoresInt.append(nRandom)
-            hi[nRandom] += 1
-        conjuntoValores.append(valoresInt) # aca se guardan los arreglos valores de cada corrida
-        conjuntoValoresHi.append(hi) # aca se guardan los arreglos de cantidad de repeticion de un valor por corrida
+            nRandom = ran.randint (0,36)    # genera un numero aleatorio entre 0 y 36
+            valoresInt.append(nRandom)      # agrega el numero aleatorio a un arreglo
+            hi[nRandom] += 1                # aumenta la frecuencia relativa del numero aleatorio
+        conjuntoValores.append(valoresInt)  # se le agrega a conjuntoValores los valores aleatorios de cada tirada
+        conjuntoValoresHi.append(hi)        # aca se guardan los arreglos de cantidad de repeticion de un valor por corrida
 
-def funcionProm(repeticiones, corridas, conjuntoValores):
-    #Grafica de los promedios
+def funcionProm(repeticiones, corridas, conjuntoValores):   # Grafica de los promedios
     for c in range(corridas):
         x = []; y= []
         suma=0
-        valoresxCorrida = conjuntoValores[c]
+        valoresxCorrida = conjuntoValores[c]        
         for i in range(repeticiones):
-            suma += valoresxCorrida[i]
-            x.append(i)
-            y.append(suma/(i+1))
-        plt.plot(x,y)
+            suma += valoresxCorrida[i]  # suma los valores de cada corrida
+            x.append(i)             
+            y.append(suma/(i+1))        # suma/(i+1) es el promedio de los valores de la corrida
+        plt.plot(x,y)                   # grafica el promedio de los valores de la corrida
     plt.xlabel("Numero de tiradas")
     plt.ylabel("Promedio")
     plt.title("Evaluacion del promedio sobre el conjunto de valores aleatorios")
     plt.axhline(y=18, color = "r")  
     plt.ylim([12, 25]) # esta ponderada la medicion
 
-def funcionPromProm(corridas, conjuntoValores):
-    #Grafica del promedio de los promedios
+def funcionPromProm(corridas, conjuntoValores): # Grafica del promedio de los promedios
     x = []; y= []
     for c in range(corridas):
         valoresxCorrida = conjuntoValores[c]
@@ -49,32 +43,30 @@ def funcionPromProm(corridas, conjuntoValores):
     plt.ylabel("Promedio")
     plt.title("Evaluacion de la media aritmetica sobre el conjunto de valores aleatorios")
     plt.axhline(y=18, color = "black")  
-    plt.grid()
+    plt.grid() 
     plt.plot(x,y, color='r')
 
-def funcionHi(repeticiones, corridas, conjuntoValoresHi):
-    #Grafica de las frec relativas
+def funcionHi(repeticiones, corridas, conjuntoValoresHi):   # Grafica de las frecuencias relativas
     for c in range(corridas):
         x = []; y= []
-        valoresxCorrida = conjuntoValoresHi[c]
+        valoresxCorrida = conjuntoValoresHi[c]  
         for i in range(36):
             x.append(i)
             y.append(valoresxCorrida[i]/repeticiones) # divido la cantidad de veces que salio un numero por la cantidad de repeticiones de la corrida
-        plt.bar(x,y, alpha=0.4)
+        plt.bar(x,y, alpha=0.4) # grafica las frecuencias relativas de cada numero de la ruleta en cada corrida
     plt.xlabel("Numero de tiradas")
     plt.ylabel("Frecuencia Relativa")
     plt.title("Evaluacion de la frecuencia relativa sobre el conjunto de valores aleatorios")
     plt.axhline(y=0.027, color = "r")  
-    plt.ylim([0.01, 0.0425]) # esta ponderada
+    plt.ylim([0.01, 0.0425]) # está ponderada
 
-def funcionPuntos(repeticiones, corridas, conjuntoValores):
-    #Grafica de las frec relativas de puntos 
+def funcionPuntos(repeticiones, corridas, conjuntoValores): # Grafica de las frecuencias relativas de puntos
     for c in range(corridas):
         x = []; y= []
         valoresxCorrida = conjuntoValores[c]
         for i in range(repeticiones):
             x.append(i)
-            y.append(valoresxCorrida[i]) ## grafica el numero que salio en cada tirada de la corrida para cada corrida
+            y.append(valoresxCorrida[i]) # grafica el numero que salio en cada tirada de la corrida para cada corrida
         plt.scatter(x,y)
     plt.xlabel("Numero de tiradas")
     plt.ylabel("Frecuencia Relativa")
@@ -82,10 +74,10 @@ def funcionPuntos(repeticiones, corridas, conjuntoValores):
 
 def RuletaVarianza(tir, corr, conjuntoValores):
     for c in range(corr):
-        y = []; x = []; valoresi = []
-        valoresxCorrida = conjuntoValores[c]
+        y = []; x = []; valoresi = []   
+        valoresxCorrida = conjuntoValores[c]    
         for i in range(tir):
-            valoresi.append(valoresxCorrida[i])
+            valoresi.append(valoresxCorrida[i])    
             x.append(i+1)  
             y.append(np.var(valoresi)) # esta funcion hace la varianza del arreglo de valores
         plt.plot(x, y)
@@ -107,7 +99,7 @@ def RuletaDesvio(tir, corr, conjuntoValores):
         plt.plot(x, y)
     plt.xlabel("Numero probable")
     plt.ylabel("Resultados del tiro")   
-    plt.axhline(y=np.sqrt(114), color = "black")  
+    plt.axhline(y=np.sqrt(114), color = "black")    
     plt.title("Evaluacion del desvio estandar sobre el conjunto de valores aleatorios")
     plt.ylim([5, 16]) # esta ponderada la medicion
     plt.show()
@@ -116,7 +108,7 @@ def RuletaDesvio(tir, corr, conjuntoValores):
 rep = int(input("Ingrese la cantidad de TIRADAS que quiere ejecutar: "))
 corr = int(input("Ingrese las CORRIDAS: "))
 
-#Generar conjuntos de valores random
+#Generar conjuntos de valores random 
 funcion(rep, corr)
 
 #Graficas
